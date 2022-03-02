7c96a8b18d7da73a2c402a4266d9b9ae7ec45a08
muabui1012 <nghu15@gmail.com>
2022-03-02 22:57:10 +0700

    Message

--- main.cpp
+++ main.cpp
@@ -3,8 +3,51 @@
 
 using namespace std;
 
+void logSDLError(std::ostream& os,
+                const std::string &msg, bool fatal = false);
+
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const string WINDOW_TITLE = "An Implementation of Code.org Painter";
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer);
+
+
+
 int main(int argc, char* argv[]){
     cout << "HELLO";
 
+
     return 0;
+}
+
+
+
+
+void logSDLError(std::ostream& os,
+        const std::string &msg, bool fatal)
+{
+    os << msg << " Error: " << SDL_GetError() << std::endl;
+    if (fatal) {
+        SDL_Quit();
+        exit(1);
+    }
+}
+
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+        logSDLError(std::cout, "SDL_Init", true);
+    window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    //window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED,
+    //SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
+
+    if (window == nullptr) logSDLError(std::cout, "CreateWindow", true);
+    //Khi chạy trong môi trường bình thường (không chạy trong máy ảo)
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    //Khi chạy ở máy ảo (ví dụ tại máy tính trong phòng thực hành ở trường)
+    //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
+
+    if (renderer == nullptr) logSDLError(std::cout, "CreateRenderer", true);
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
 }
